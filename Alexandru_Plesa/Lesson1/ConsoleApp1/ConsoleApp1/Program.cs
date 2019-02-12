355aae7328965e9a41f6238aca442e1e721379a4
Alexandru Plesa <aplesa@sdl.com>
2019-02-12 14:59:07 +0200

    comit 3

--- Alexandru_Plesa/Lesson1/ConsoleApp1/ConsoleApp1/Program.cs
+++ Alexandru_Plesa/Lesson1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -43,10 +43,100 @@ namespace ConsoleApp1
             int ii2 = 13 % 2;
             double dd1 = 13 / 3;
 
-
-
             Console.WriteLine($"{ii1} {ii2} {dd1}");
+
+            bool b11 = false;
+            bool b12 = !b11;
+            Console.WriteLine("truth value: {0}", b12);
+
+            if (!b11)
+            {
+
+            }
+            else
+            {
+
+            }
+
+            int ii3 = 5;
+            int result = ii3 % 2;
+            if (result != 0)
+            {
+                Console.WriteLine($"{ii3} is odd");
+            } else
+            {
+                Console.WriteLine($"{ii3} is even");
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                if ((i % 2) == 0)
+                {
+                    Console.WriteLine($"{i} is a even number");
+                }
+            }
+
+            int j = 0;
+            while (j < 10)
+            {
+                Console.WriteLine($"{j}");
+                j++;
+            }
+
+            Medals medals = Medals.Silver;
+
+            switch (medals)
+            {
+                case Medals.Silver:
+                    Console.WriteLine($" nice {Medals.Silver}");
+                    break;
+                case Medals.Gold:
+                    Console.WriteLine($" nice {Medals.Gold}");
+                    break;
+                default:
+                    Console.WriteLine("This is the default value");
+                    return;
+            }
+
+            int result1 = AddTwoNumbers(9, 10);
+            Console.WriteLine(result1);
+
+            int result2 = Sum(10);
+            Console.WriteLine(result2);
+
+            int result3 = SumIterative(11);
+            Console.WriteLine(result3);
+
             Console.ReadLine();
         }
+        public static int AddTwoNumbers(int a, int b)
+        {
+            int result = a + b;
+            return result;
+        }
+
+        public static int Sum(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+
+            return n + Sum(n - 1);
+        }
+
+        public static int SumIterative(int n)
+        {
+            int finalValue = 0;
+            while (n >= 0)
+            {
+                finalValue += n;
+                n--;
+            }
+
+            return finalValue;
+        }
+
     }
+    
 }
