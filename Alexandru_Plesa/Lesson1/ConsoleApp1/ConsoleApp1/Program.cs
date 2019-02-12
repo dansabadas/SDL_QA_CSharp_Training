6723281efc7c216c5480b0ce807dbb054a55d595
Alexandru Plesa <aplesa@sdl.com>
2019-02-12 14:20:29 +0200

    some more fundamentals

--- Alexandru_Plesa/Lesson1/ConsoleApp1/ConsoleApp1/Program.cs
+++ Alexandru_Plesa/Lesson1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,6 +2,14 @@
 
 namespace ConsoleApp1
 {
+    public enum Medals
+    {
+        Bronze = 2,
+        Silver = 6,
+        Gold,
+        Platinum
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -15,6 +23,30 @@ namespace ConsoleApp1
             float f1 = 2.5f;
             var f2 = 3.5;
             Console.WriteLine($"{f1} {f2}");
+
+            char c1 = 'd', c2 = 'E';
+            Console.WriteLine($"{c1} {(int)c1} {c2} {(int)c2}");
+
+            bool b1 = true, b2 = false;
+            Console.WriteLine($"{b1} {b2}");
+
+            byte by1 = 13, by2 = 255;
+            Console.WriteLine($"{by1} {by2}");
+
+            Medals m1 = Medals.Gold;
+            Console.WriteLine($"{m1} {(int)m1}");
+
+            string s1 = "Hello World!";
+            Console.WriteLine(s1);
+
+            int ii1 = 13 / 3;
+            int ii2 = 13 % 2;
+            double dd1 = 13 / 3;
+
+
+
+            Console.WriteLine($"{ii1} {ii2} {dd1}");
+            Console.ReadLine();
         }
     }
 }
