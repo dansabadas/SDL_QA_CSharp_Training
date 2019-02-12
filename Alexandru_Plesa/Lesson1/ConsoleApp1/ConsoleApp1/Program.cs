4577a803ccb77e804b85c0b6ec0450960bdf51a6
Alexandru Plesa <aplesa@sdl.com>
2019-02-12 13:43:26 +0200

    Some changes

--- Alexandru_Plesa/Lesson1/ConsoleApp1/ConsoleApp1/Program.cs
+++ Alexandru_Plesa/Lesson1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,15 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello I'm Dan");
+            Console.WriteLine("Hello I'm Alex");
+            int i1 = 2;
+            int i2 = 0;
+            int i3 = 5;
+            Console.WriteLine($"{i2} {i2}");
+
+            float f1 = 2.5f;
+            var f2 = 3.5;
+            Console.WriteLine($"{f1} {f2}");
         }
     }
 }
