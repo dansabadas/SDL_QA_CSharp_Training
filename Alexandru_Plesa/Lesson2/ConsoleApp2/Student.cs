04d84f186f7b0911919e5839cfad989f6011eab1
Alexandru Plesa <aplesa@sdl.com>
2019-02-14 13:01:31 +0200

    commit

---
+++ Alexandru_Plesa/Lesson2/ConsoleApp2/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Student
+    {
+        public int Id;
+        public double Average;
+        public string Name;
+        public DateTime birthDate;
+    }
+}
