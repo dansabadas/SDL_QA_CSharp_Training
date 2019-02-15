acbc4ff97ee15f8be7abfc5e394a7447274a42fe
Raphael Batel <rap.spm@web.de>
2019-02-15 22:09:49 +0200

    Sort done

--- Raphael_Batel/Lesson1/ConsoleApp1/Homework1/Program.cs
+++ Raphael_Batel/Lesson1/ConsoleApp1/Homework1/Program.cs
@@ -10,6 +10,12 @@ namespace Homework1
     {
         static void Main(string[] args)
         {
+            int[] array = { 7 , 3, 5, 6, 0, 10, 4 , 6, 0, 34954, 456, 100, 254, 43534, 4554, 665,44,0, -4, 5, 66, 666};
+            int[] b = Sort(array);
+            foreach (var i in b)
+            {
+                Console.WriteLine(i);
+            }
         }
 
         static int[] Sort(int[] array)
@@ -21,17 +27,39 @@ namespace Homework1
         static void Sort(int[] array, int beginIndex, int endIndexExcluding)
         {
             int lengthToSort = endIndexExcluding - beginIndex;
-            if (lengthToSort == 1)
+            if (lengthToSort == 1 || lengthToSort == 0)
                 return;
 
-            int pivotIndex = Partition(new ArraySegment<int>(array, beginIndex, lengthToSort));
-            Sort(array, 0, pivotIndex);
+            int pivotIndex = beginIndex + Partition(array, beginIndex, endIndexExcluding);
+            Sort(array, beginIndex, pivotIndex);
             Sort(array, pivotIndex + 1, array.Length);
         }
 
-        private static int Partition(ArraySegment<int> arraySegment)
+        private static int Partition(int[] array, int beginIndex, int endIndexExcluding)
         {
-            throw new NotImplementedException();
+            int lengthToSort = endIndexExcluding - beginIndex;
+
+            if (lengthToSort <= 0)
+                throw new ArgumentException("The subarray to partition contains no elements.");
+
+            if (lengthToSort == 1)
+                return 0;
+
+            if (array[beginIndex] <= array[beginIndex + 1])
+            {
+                Swap(array, beginIndex + 1, endIndexExcluding - 1);
+                return Partition(array, beginIndex, endIndexExcluding - 1);
+            }
+            else
+            {
+                Swap(array, beginIndex, beginIndex + 1);
+                return 1 + Partition(array, beginIndex + 1, endIndexExcluding);
+            }
+        }
+
+        private static void Swap(int[] array, int index1, int index2)
+        {
+            (array[index1], array[index2]) = (array[index2], array[index1]);
         }
     }
 }
