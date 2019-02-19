652702dfe97470cf323b3e3d23c668c477aeed04
Adrian <danciu2010@yahoo.com>
2019-02-19 21:07:08 +0200

    HomeWork2

--- Adrian_Danciu/HomeWork/HomeWork/Program.cs
+++ Adrian_Danciu/HomeWork/HomeWork/Program.cs
@@ -20,6 +20,13 @@ namespace HomeWork
             {
                 Console.Write(sorted[i] + " ");
             }
+            Console.WriteLine();
+
+            //Homework: 2.Implement Fibonacci iteratively, not recursively
+            int fiboResult = Fibonacci(5);
+
+            Console.WriteLine(fiboResult);
+                                        
         }
 
         public static int[] MySort (int[] unsorted)
@@ -41,5 +48,29 @@ namespace HomeWork
 
             return unsorted;
         }
+
+        public static int Fibonacci(int n)
+        {
+            if ((n == 0) || (n == 1))
+            {
+                return n;
+            }
+            int b = 0;
+            int a = 1;
+            int i = 2;
+            int currentValue = a + b;
+            while(i<=n)
+            {
+                int temp1 = a;
+                int temp2 = currentValue;
+                currentValue = currentValue + a;
+                a = temp2;
+                b = temp1;
+
+                i++;
+            }
+            return currentValue;
+        }
+
     }
 }
