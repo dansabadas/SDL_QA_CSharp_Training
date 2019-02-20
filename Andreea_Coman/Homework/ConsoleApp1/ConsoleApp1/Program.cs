e530f38f4f696242a7e2e605294ec60ed88788cf
acoman <acoman@CLJQAACOMAN01>
2019-02-20 13:27:18 +0200

    o parte din tema + lesson 3

---
+++ Andreea_Coman/Homework/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("introduceti un numar pentru care sa afisam sirul fibonacci: ");
+            var n = Console.Read();
+            Console.WriteLine("rezultate");
+            FibonacciIterativ(n);
+
+        }
+
+        public static int FibonacciIterativ(int fib)
+        {
+            int suma = 0;
+            int n1 = 0;
+            int n2 = 1;
+            suma = n1 + n2;
+            Console.WriteLine("rezultate");
+            Console.WriteLine($"0 1 {suma}", suma);
+           
+            for (int i = 2; i <= fib; i++)
+            {
+                suma = n1 + n2;
+                Console.WriteLine($"{suma}", suma);
+                n1 = n2;
+                n2 = suma;
+
+            }
+            return suma;
+        }
+    }
+}
---
+++ Andreea_Coman/Lesson3/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+
+namespace ConsoleApp1
+{
+    public class Student
+    {
+        public int id;
+        public string name;
+        public DateTime dateOfBirth;
+    }
+
+    public class StudentRepository
+    {
+        public ArrayList CreateAllStudents()
+        {
+            ArrayList myList = new ArrayList();
+            for (int i = 1; i < 10; i++)
+            {
+                Student myStudent = new Student();
+                myStudent.id = i;
+                myStudent.name = "student name" + i;
+                myStudent.dateOfBirth = DateTime.Today.AddYears(-i);
+                myList.Add(myStudent);
+                Console.WriteLine($"name { myStudent.name} data nasterii {myStudent.dateOfBirth}" );
+            }
+            return myList; 
+        }
+        
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StudentRepository myRepository= new StudentRepository();
+            ArrayList myList = myRepository.CreateAllStudents();
+
+            Console.ReadKey();
+
+          //   Console.WriteLine("lesson3");
+        }
+    }
+}
