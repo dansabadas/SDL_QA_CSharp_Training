51735bcadae5ccfa54d875d3e237d4f6b078bf4a
Alexandru Plesa <aplesa@sdl.com>
2019-02-20 13:04:06 +0200

    bla

---
+++ Alexandru_Plesa/Lesson3/ConsoleApp3/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class Student
+    {
+        public string Name;
+        public int ID;
+        public DateTime DateOfBirth;
+    }
+
+    public class StudentRepository
+    {
+        public ArrayList GetAllStudents(int nr)
+        {
+            ArrayList students = new ArrayList();
+
+            for (int i = 0; i < nr; i++)
+            {
+                Student student = new Student();
+
+                student.ID = i;
+                student.Name = "Some Name" + i;
+                student.DateOfBirth = DateTime.Now.AddYears(-i);
+
+                students.Add(student);
+            }
+            
+            return students;
+        }
+    }
+    class Program
+    {
+    
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            StudentRepository studentRepository = new StudentRepository();
+
+            ArrayList students = studentRepository.GetAllStudents(100);
+
+            foreach(Student stud in students)
+            {
+                Console.WriteLine($"Student {stud.Name} has the ID {stud.ID} and was born {stud.DateOfBirth}");
+            }
+
+            Console.WriteLine("Hello");
+            Console.ReadLine();
+            
+            
+        }
+    }
+}
