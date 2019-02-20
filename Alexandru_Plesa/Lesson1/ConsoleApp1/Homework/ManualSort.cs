60eb7acfcaa276c5d24afd737a9da0245c605abc
Alexandru Plesa <aplesa@sdl.com>
2019-02-20 10:51:19 +0200

    Created a separate project for the Homework

---
+++ Alexandru_Plesa/Lesson1/ConsoleApp1/Homework/ManualSort.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework
+{
+    public class ManualSort
+    {
+        public int[] ManualSortArray(int[] myarray)
+        {
+            for (int i = 0; i < myarray.Length - 1; i++)
+            {
+                int smallNumber = myarray[i];
+                int indexOfSmallNumber = i;
+
+                for (int j = i + 1; j < myarray.Length; j++)
+                {
+                    if (myarray[j] < smallNumber)
+                    {
+                        smallNumber = myarray[j];
+                        indexOfSmallNumber = j;
+                    }
+                }
+
+                myarray[indexOfSmallNumber] = myarray[i];
+                myarray[i] = smallNumber;
+            }
+            return myarray;
+        }
+    }
+}
