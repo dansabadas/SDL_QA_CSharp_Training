c7b66dc92b47f8569473fe890a64759d9dd2c36c
acoman <acoman@CLJQAACOMAN01>
2019-02-24 15:25:32 +0200

    distanta dintre 2 puncte 2D

---
+++ Andreea_Coman/Homework2/Homework2/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+
+namespace Homework_ex1
+
+{
+    public class point2D
+    {
+        private double _x;
+        private double _y;
+
+        public double X
+        {
+            get { return _x; }
+        }
+        public double Y => _y;
+
+        public point2D(double x, double y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public double DistanceTO(double a, double b)
+        {
+            double distance = Math.Sqrt((a - _x) * (a - _x) + (b - _y) * (b - _y));
+            return distance;
+        }
+    }
+      
+    public interface IGeometryFactory
+    {
+        ArrayList GetMyPoints();
+        ArrayList GetRandomPoint(int maxX, int maxY);
+      //  ArrayList GetRandomPoint3D(int x, int y, int z);
+    }
+
+    public class Points : IGeometryFactory
+    {
+        public ArrayList GetMyPoints()
+        {
+            // generez o lista de numere random cu 2 dimensiuni 
+            ArrayList myArrayList = new ArrayList();
+            for (int i = 0; i < 5; i++)
+            {
+                Random random = new Random();
+                point2D point = new point2D(random.NextDouble(), random.NextDouble());
+                myArrayList.Add(point); // imi afiseaza de 5 ori aceleasi numere 
+            }
+            return myArrayList;
+        }
+
+        public ArrayList GetRandomPoint(int maxX, int maxY)
+        {
+            ArrayList myList2 = new ArrayList();
+            for (int i = 0; i < 5; i++)
+            {
+                Random random = new Random();
+                point2D point = new point2D(random.NextDouble() * maxX, random.NextDouble() * maxY); // transform numerele obtinute anterior 
+                myList2.Add(point);
+            }
+            return myList2;
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                //Points PunctGeometric = new Points();
+
+                //ArrayList myListOfPoints = PunctGeometric.GetMyPoints();
+                //foreach (point2D punct in myListOfPoints)
+                //{
+                //    Console.WriteLine($"{punct.X}, {punct.Y}"); // afisez prima lista, cea din GetMyPoints
+                //}
+
+                //ArrayList SecondList = PunctGeometric.GetRandomPoint(120, 5);
+                //foreach (point2D punct in SecondList)
+                //{
+                //    Console.WriteLine($"{punct.X}, {punct.Y}");  // afisez a doua lista, cea cu numerele diferite, din GetRandomPoint
+                //}
+
+                Console.WriteLine("Calculam distanta dintre doua numere 2D");
+                Console.WriteLine("cream primul punct ");                           
+                point2D myFisrtPoint2D = new point2D(12, -14);
+                Console.WriteLine($"{ myFisrtPoint2D.X},{ myFisrtPoint2D.Y}");
+                
+                Console.WriteLine("cream al doilea punct");
+                point2D mySecondPoint2D = new point2D(4, 8);
+                Console.WriteLine($"{mySecondPoint2D.X}, {mySecondPoint2D.Y}");
+
+                Console.WriteLine("afisam distanta");
+                Console.WriteLine($"{myFisrtPoint2D.DistanceTO(mySecondPoint2D.X, mySecondPoint2D.Y)}");
+                             
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
+ 
--- Andreea_Coman/Lesson3/ConsoleApp1/ConsoleApp1/Program.cs
+++ Andreea_Coman/Lesson3/ConsoleApp1/ConsoleApp1/Program.cs
@@ -15,16 +15,21 @@ namespace ConsoleApp1
         private double _x;
         private double _y;
 
-        public double X {
+        public double X
+        {
             get { return _x; }
-            set { if (value < 0) {
+            set
+            {
+                if (value < 0)
+                {
                     _x = -value;
                 }
                 else if (value > 0)
-                { _x = value + 10; }
+                { _x = value + 10;
+                }
             }
         }
-        public double Y => _y; 
+        public double Y => _y;
 
         public point2D(double x, double y)
         {
@@ -34,8 +39,8 @@ namespace ConsoleApp1
 
         public class point3D : point2D
         {
-            private double _z; 
-            public point3D(double x, double y, double z): base (x,y)
+            private double _z;
+            public point3D(double x, double y, double z) : base(x, y)
 
             {
                 _z = z;
@@ -55,7 +60,7 @@ namespace ConsoleApp1
         ArrayList GetMyPoints();
         ArrayList GetRandomPoint(int maxX, int maxY);
         ArrayList GetRandomPoint3D(int x, int y, int z);
-       }
+    }
 
     public interface IStudentRepository
     {
@@ -90,16 +95,12 @@ namespace ConsoleApp1
             }
             return myList2;
         }
-
-
-
+               
         public ArrayList GetRandomPoint3D(int x, int y, int z)
         {
             point3D punct = new point3D(x, y, z);
-
-   
-
-        }
+            
+                  }
 
 
 
@@ -175,3 +176,111 @@ namespace ConsoleApp1
         }
     }
 }
+    public class Points : IGeometryFactory
+    {
+        public ArrayList GetMyPoints()
+        {
+            ArrayList myArrayList = new ArrayList();
+            for (int i = 0; i < 10; i++)
+            {
+                Random random = new Random();
+
+                point2D point = new point2D(random.NextDouble(), random.NextDouble());
+                myArrayList.Add(point);
+            }
+            return myArrayList;
+        }
+
+        public ArrayList GetRandomPoint(int maxX, int maxY)
+        {
+            ArrayList myList2 = new ArrayList();
+            for (int i = 0; i < 10; i++)
+            {
+                Random random = new Random();
+
+                point2D point = new point2D(random.NextDouble() * maxX, random.NextDouble() * maxY);
+                myList2.Add(point);
+            }
+            return myList2;
+        }
+
+        public ArrayList GetRandomPoint3D(int x, int y, int z)
+        {
+            point3D punct = new point3D(x, y, z);
+
+        }
+
+
+
+        public class StudentRepository
+        {
+            public string nameOfRepository = "some name";
+
+            public ArrayList CreateAllStudents()
+            {
+                ArrayList myList = new ArrayList();
+                for (int i = 1; i < 10; i++)
+                {
+                    Student myStudent = new Student();
+                    myStudent.id = i;
+                    myStudent.name = "student name" + i;
+                    myStudent.dateOfBirth = DateTime.Today.AddYears(-i);
+                    myList.Add(myStudent);
+                    Console.WriteLine($"name { myStudent.name} data nasterii {myStudent.dateOfBirth}");
+                }
+                return myList;
+            }
+
+            public ArrayList CreateAllStudents2()
+            {
+                ArrayList myList = new ArrayList();
+                for (int i = 1; i <= 10; i++)
+                {
+
+                    Student myStudent = new Student();
+                    myStudent.id = i;
+                    myStudent.name = "student name" + i;
+                    myStudent.dateOfBirth = DateTime.Today.AddYears(-i);
+                    if (i % 2 == 0)
+                    {
+                        myList.Add(myStudent);
+                        Console.WriteLine($"name { myStudent.name} data nasterii {myStudent.dateOfBirth}");
+                    }
+                }
+                return myList;
+            }
+
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                StudentRepository myRepository = new StudentRepository();
+                ArrayList myList = myRepository.CreateAllStudents2();
+                Console.WriteLine("lesson3");
+
+                Points PunctGeometric = new Points();
+                ArrayList myListOfPoints = PunctGeometric.GetMyPoints();
+                foreach (point2D punct in myListOfPoints)
+                {
+                    Console.WriteLine($"{punct.X}, {punct.Y}");
+                }
+
+                ArrayList SecondList = PunctGeometric.GetRandomPoint(1, 2);
+                foreach (point2D punct in SecondList)
+                {
+                    Console.WriteLine($"{punct.X}, {punct.Y}");
+                }
+
+                point2D p2 = new point2D(2, 3);
+                p2.X = -30;
+                Console.WriteLine(p2.ToString());
+                p2.X = 100;
+                Console.WriteLine(p2.ToString());
+
+                Console.ReadKey();
+
+            }
+        }
+    }
