165a20e0080d00aaa9d98bf230cb704719657228
Adrian <danciu2010@yahoo.com>
2019-02-25 21:32:47 +0200

    Homework Animal Farm

---
+++ Adrian_Danciu/HomeWork/HomeWork/Animal.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork
+{
+    public abstract class Animal
+    {
+        protected int ID { get; set; }
+        protected string Name { get; set; }
+
+        public Animal(int id, String name)
+        {
+            ID = id;
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return $"{ID},{Name}";
+        }
+
+    }
+
+    public class Pig : Animal
+    {
+        public int Weight { get; set; }
+
+        public Pig(int id, String name, int weight) : base(id, name)
+        {
+            Weight = weight;
+        }
+
+        public override string ToString()
+        {
+            return $"Pig: {ID},{Name},{Weight}";
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public int EnemiesBitten { get; set; }
+
+        public Dog(int id, String name, int enemiesBitten) : base(id, name)
+        {
+            EnemiesBitten = enemiesBitten;
+        }
+
+        public override string ToString()
+        {
+            return $"Dog: {ID},{Name},{EnemiesBitten}";
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public int MiceKilled { get; set; }
+
+        public Cat(int id, String name, int miceKilled) : base(id, name)
+        {
+            MiceKilled = miceKilled;
+        }
+
+        public override string ToString()
+        {
+            return $"Cat: {ID},{Name},{MiceKilled}";
+        }
+    }
+
+    public class Cow : Animal
+    {
+        public int MilkProduced { get; set; }
+
+        public Cow(int id, String name, int milkProduced) : base(id, name)
+        {
+            MilkProduced = milkProduced;
+        }
+
+        public override string ToString()
+        {
+            return $"Cow: {ID},{Name},{MilkProduced}";
+        }
+    }
+
+    public class AnimalFarm
+    {
+        public ArrayList GenerateAnimals()
+        {
+            ArrayList myArrayList = new ArrayList();
+            for (int i = 0; i < 100; i++)
+            {
+                Animal animal = null;
+                switch (i % 4)
+                {
+                    case 0:
+                        animal = new Pig(i, "pig" + i, new Random().Next(100, 200));
+                        break;
+                    case 1:
+                        animal = new Dog(i, "dog" + i, new Random().Next(90, 100));
+                        break;
+                    case 2:
+                        animal = new Cat(i, "cat" + i, new Random().Next(50, 70));
+                        break;
+                    case 3:
+                        animal = new Cow(i, "cow" + i, new Random().Next(20, 30));
+                        break;
+                }
+                myArrayList.Add(animal);
+            }
+            return myArrayList;
+        }
+    }
+}
+
--- Adrian_Danciu/HomeWork/HomeWork/Program.cs
+++ Adrian_Danciu/HomeWork/HomeWork/Program.cs
@@ -41,7 +41,7 @@ namespace HomeWork
                 Console.WriteLine(s.Name);
             }
 
-            //Homework 2 to the classes Point2D and Point3d that you created add for each class one 
+            //Homework 2.1 to the classes Point2D and Point3d that you created add for each class one 
             //new method double CalculateDistanceTo(Point2D/Point3D) which will calculate the physical 
             //distance between two points! It is just basic trigonometry! 😄 Then on the main function 
             //please create 2 random 2D points and calculate the distance between them using a call like this:
@@ -54,7 +54,31 @@ namespace HomeWork
             Console.WriteLine("myFirstpoint2d is:" + myFirstpoint2d + " " + "mySecondpoint2d is:" + mySecondpoint2d);
 
             Console.WriteLine("The distance between two points is: " + myFirstpoint2d.CalculateDistanceTo(mySecondpoint2d));
-                          
+
+            //Homework 2.1 The Animal farm! 😄
+            //            Create an abstract class Animal. Even though I did not explain what an abstract class is, please just write public abstract class Animal {...}
+            //        and that would do it!. Add inside that class declaration for 2 properties: int ID and string Name which are protected. Override the ToString() method to return a string with the ID and Name
+            //Also create a protected constructor that takes as arguments an int id and a string name and populate the properties with the parameters passed in the constructor.
+            //2. create a class Pig which inherits from Animal and adds an additional public property int Weight { get; set; }
+            //3. create a class Dog which inherits : from Animal and that adds an additional public property int EnemiesBitten
+            //4. create a class Cat which inherits from Animal and declares/adds an additional public property int MiceKilled 
+            //5. create a class Cow which inherits from Animal and declares an additional public property int MilkProduced
+            //6. For all four concrete classes created at points 2-5 override their ToString methods to return the Type of the class and the ToString p////roperty 
+            //defined in Animal(for Dog class should be something like this: return "Dog " + base.ToString();
+            //7. Create a class AnimalFarm with one method ArrayList GenerateAnimals() which will return an array list with 100 animals inside! those 
+            // with ID divisible with 4 will be Pigs, those with the remainder of ID divided by 4 equal to 1 will be Dogs , those with the remainder of ID divided by 
+            //4 equal to 2 will be Cats and those with the remainder of ID divided by 4 equal to 3 will be Cows.Add into each object of the concrete
+            //class (Pigs, Dogs Cats and Cows) into their specific property a random number for Weight, EnemiesBitten and so on.
+            //8. invoke for each animal created at point 7 with GenerateAnimals() their ToString() methods!
+
+            AnimalFarm animalFarm = new AnimalFarm();
+            ArrayList animalList = animalFarm.GenerateAnimals();
+
+            foreach (Animal animal in animalList)
+            {
+                Console.WriteLine(animal);
+            }
+
         }
 
         public static int[] MySort (int[] unsorted)
