e71efeab24e940d39ff71a83d993b4aa648ca89e
Dan Sabadis <dsabadis@sdl.com>
2019-02-28 13:06:26 +0200

    lesson 5 done

---
+++ Dan_Sabadis/Lesson5/ClassLibrary1/Soldier.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Soldier
+    {
+        public int Id { get; }
+
+        public string Name { get; }
+
+        public int CountryId { get; set; }
+
+        public int NumberOfKills { get; set; }
+
+        public Soldier(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return $"({Id}, {Name}, {CountryId}, {NumberOfKills})";
+        }
+    }
+}
---
+++ Dan_Sabadis/Lesson5/ConsoleApp1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+
+namespace ConsoleApp1
+{
+    public class Student
+    {
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList ar1 = new ArrayList();
+            ar1.Add(1);
+            ar1.Add("hello world");
+            ar1.Add(DateTime.Now.Date);
+            ar1.Add(new Student());
+
+            Console.WriteLine($"{ar1.Count}");
+
+            var retrievedData = (DateTime)ar1[2];
+
+            List<string> stringList1 = new List<string>();
+            stringList1.Add("hello 1");
+            List<int> intList2 = new List<int>() { 1, 2, 3};
+
+            List<Student> myStudentList3 = new List<Student>() {new Student() };
+            myStudentList3.Add(new Student());
+            Student studentRetrieved = myStudentList3[1];
+
+            IArmy myFirstArmy = new Army();
+            foreach (var soldier in myFirstArmy.RetrieveSoldiers())
+            {
+                Console.WriteLine(soldier);
+            }
+
+            foreach (var country in myFirstArmy.GetCountries())
+            {
+                Console.WriteLine(country);
+            }
+
+            foreach (var soldier in myFirstArmy.GetChineseSoldiers())
+            {
+                Console.WriteLine(soldier);
+            }
+
+            foreach (var soldier in myFirstArmy.GetTopFiveSoldiers())
+            {
+                Console.WriteLine(soldier);
+            }
+
+            foreach (var soldierReport in myFirstArmy.GetFullReport())
+            {
+                Console.WriteLine(soldierReport);
+            }
+
+            Console.WriteLine($"lethality={myFirstArmy.GetLethality()}");
+        }
+    }
+}
