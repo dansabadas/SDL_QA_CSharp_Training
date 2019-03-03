82bca566626805452dcd49a1a922916dd45a9147
AlexPlesa <plesa.alecs@gmail.com>
2019-03-03 18:39:21 +0200

    asd

--- Alexandru_Plesa/Army Homework/Army/ArmyReportig/Program.cs
+++ Alexandru_Plesa/Army Homework/Army/ArmyReportig/Program.cs
@@ -21,20 +21,39 @@ namespace ArmyReportig
             List<Country> countries = army.GetCountries();
             List<Soldier> soldiers = army.RetrueveSoldiers();
 
-            foreach (Soldier soldier in soldiers)
-            {
-                Console.WriteLine(soldier);
-            }
+            printSoldiers(soldiers);
 
             Country romania = countries.FirstOrDefault(country => country.ID == 2);
             Country unitedKingdom = countries.FirstOrDefault(country => country.ID == 3);
             Country unitedStates = countries.FirstOrDefault(country => country.ID == 1);
 
-            List<Soldier> uKsoldiers = army.GetSoldiersByCountry(soldiers, unitedKingdom);
-            Soldier mostLethalRomanianSoldier = army.GetTheMostLethalSoldierFromCountry(soldiers, romania);
-            double averageNumberOfKillsForUsaSildiers = army.GetAverageNumberOfKillsFromCountry(soldiers, unitedStates);
+            List<Soldier> UkSoldiers = army.GetSoldiersByCountry(soldiers, unitedKingdom);
+            printSoldiers(UkSoldiers);
 
-            Console.WriteLine("");
+            List<Soldier> UsaSoldiers = army.GetSoldiersByCountry(soldiers, unitedStates);
+            printSoldiers(UsaSoldiers);
+
+            Soldier mostLethalRomanianSoldier = army.GetTheMostLethalSoldierFromCountry(soldiers, romania);
+            printSoldiers(mostLethalRomanianSoldier);
+
+
+            double averageNumberOfKillsForUsaSildiers = army.GetAverageNumberOfKillsFromCountry(soldiers, unitedStates);
+            Console.WriteLine(averageNumberOfKillsForUsaSildiers);
+
+            Console.ReadLine();
+        }
+
+        static void printSoldiers(List<Soldier> soldiers)
+        {
+            foreach (Soldier soldier in soldiers)
+            {
+                Console.WriteLine(soldier);
+            }
+        }
+
+        static void printSoldiers(Soldier soldier)
+        {
+            Console.WriteLine(soldier);
         }
     }
 }
