7338b40f924f87581f22086729029a6b71738ae8
Alin Soritau <asoritau@sdl.com>
2019-03-04 11:44:44 +0200

    HomeWork 5 done

---
+++ HomeWork5/HomeWork5/ConsoleApp1/Program.cs
@@ -0,0 +1,65 @@
+﻿using ArmyLayer;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArmyReporting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunSamples();
+
+            Console.Read();
+        }
+
+        public static void RunSamples()
+        {
+            IArmy myFirstArmy = new Army();
+            foreach(var soldier in myFirstArmy.RetrieveSoldiers())
+            {
+                Console.WriteLine(soldier);
+            }
+
+            foreach(var country in myFirstArmy.GetCountries())
+            {
+                Console.WriteLine(country);
+            }
+
+            foreach (var soldier in myFirstArmy.GetChineseSoldiers())
+            {
+                Console.WriteLine(soldier);
+            }
+
+            foreach (var soldier in myFirstArmy.GetTopFiveSoldiers())
+            {
+                Console.WriteLine(soldier);
+            }
+
+            foreach (var soldier in myFirstArmy.GetFullReport())
+            {
+                Console.WriteLine(soldier);
+            }
+
+            foreach (var soldier in myFirstArmy.GetFullReport())
+            {
+                Console.WriteLine(soldier);
+            }
+
+            Console.WriteLine($"Lethality= {myFirstArmy.GetLethality()}");
+
+            Console.WriteLine(myFirstArmy.GetTheMostLethalRomaniaSoldier());
+
+            Console.WriteLine($"English soldiers: {myFirstArmy.GetNumberOfEnglishSoldiers()}");
+
+            Console.WriteLine($"Average kills per US Soldier: {myFirstArmy.GetAverageNumberOfKillsForAmericanSoldiers()}");
+
+            Country setCountry = new Country(1, "USA");
+            Console.Write($"Average lethality for {setCountry.Name} soldiers: {myFirstArmy.GetAverageNumberOfKillsForSoldiersBelongingToCountry(setCountry)}");
+        }
+    }
+}
