a69aadb673bde6208685304c3390f5b86d07c540
Vlad Nedelea <nedeleavlad28@outlook.com>
2019-03-04 16:14:24 +0200

    Homework for lesson 5.

---
+++ Vlad_Nedelea/Homework4/ArmyLayer/ArmyLayer/IArmy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArmyLayer
+{
+    public interface IArmy
+    {
+        List<Soldier> RetrieveSoldiers();
+
+        List<Country> RetrieveCountries();
+
+        List<Soldier> GetTop();
+
+        List<Soldier> GetChina();
+
+        List<dynamic> GetFull();
+
+        int GetLethality();
+
+        Soldier GetTheMostLethalRomaniaSoldier();
+
+        int GetNumberOfEnglishSoldiers();
+
+        double GetAverageNumberOfKillsForAmericanSoldiers();
+
+        double GetAverageNumberOfKillsForSoldiersBelongingToCountry(Country country);
+    }
+}
---
+++ Vlad_Nedelea/Homework4/ArmyReporting/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArmyLayer;
+using ClassLibrary1;
+
+namespace ArmyReporting
+{
+    class Program
+    {
+        public static void RunSamples()
+        {         
+            IArmy army1 = new Army();
+
+            var army2 = army1.RetrieveSoldiers();
+            var army3 = army1.RetrieveCountries();
+            foreach (object o in army2)
+            {
+                Console.WriteLine(o.ToString());
+            }
+
+            foreach (object o in army3)
+            {
+                Console.WriteLine(o.ToString());
+            }
+
+            foreach (var o in army1.GetTop())
+            {
+                Console.WriteLine(o.Name);
+            }
+
+            //Homewrok
+
+            Soldier s1 = army1.GetTheMostLethalRomaniaSoldier();
+
+            Console.WriteLine("Most lethal romania soldier is "+s1);
+
+            int numberOfEnglishSoldiers = army1.GetNumberOfEnglishSoldiers();
+
+            Console.WriteLine("Number of english soldiers is " + numberOfEnglishSoldiers);
+
+            double  americanKills = army1.GetAverageNumberOfKillsForAmericanSoldiers();
+
+            Console.WriteLine("Average Number Of Kills For American Soldiers is " + americanKills);
+
+            double allAverage = army1.GetAverageNumberOfKillsForSoldiersBelongingToCountry(new Country(1, "USA"));
+
+            Console.WriteLine("Average nr of kills belonging to country is " + allAverage);
+
+        }
+
+        static void Main(string[] args)
+        {
+            RunSamples();
+        }
+    }
+}
