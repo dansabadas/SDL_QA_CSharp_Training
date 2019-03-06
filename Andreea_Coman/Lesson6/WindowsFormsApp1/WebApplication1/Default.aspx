731e8951f675debeebb6653cd2acd3b840444ac6
acoman <acoman@CLJQAACOMAN01>
2019-03-06 14:33:41 +0200

    First hour of code

---
+++ Andreea_Coman/Lesson6/WindowsFormsApp1/WebApplication1/Default.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication1._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    
+    <asp:Panel ID="Panel1" runat="server"> 
+
+    <asp:Button ID="Button1" runat="server" Height="42px" OnClick="Button1_Click" Text="Do not press " Width="166px" />
+    <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+      
+
+    <asp:CheckBox ID="CheckBox1" runat="server" />
+        <asp:ImageMap ID="ImageMap1" runat="server" Height="55px" Width="212px">
+
+     </asp:ImageMap>
+             
+
+  </asp:Panel>
+   <asp:Button ID="Button4" runat="server" OnClick="Button4_Click" Text="Button" Height="34px" Width="167px" />
+     <br />
+     <asp:Panel ID="Panel2" runat="server"> 
+    <asp:Button ID="Button2" runat="server" Height="31px" OnClick="Button2_Click" Text="Button" Width="168px" />
+    
+    <br />
+    <asp:Label ID="Label2" runat="server" Text="Label2"></asp:Label>
+           </asp:Panel>
+    <br />
+    <asp:ListBox ID="ListBox1" runat="server">
+        <Items>
+                        <asp:ListItem runat="server" Text="One" Value =" 1 "></asp:ListItem>
+                        <asp:ListItem runat="server" Text="Two" Value =" 2 "></asp:ListItem>
+                        <asp:ListItem runat="server" Text="Three" Value =" 3 "></asp:ListItem>
+                        <asp:ListItem runat="server" Text="Four" Value =" 4 "></asp:ListItem>
+        </Items>
+
+    </asp:ListBox>
+
+    <br />
+    <br />
+    <p>
+    </p>
+
+    
+
+</asp:Content>
---
+++ Andreea_Coman/Lesson6/WindowsFormsApp1/WebApplication1/Default.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            this.Button1.Text = "I was pressed";
+            int MyClicks = 0;
+           
+            if (Session["MyClicks"] != null)
+            {
+                 MyClicks =(int) Session["MyClicks"];
+                
+            }
+            this.Label1.Text = (++MyClicks).ToString();
+            Session["MyClicks"] = MyClicks;
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            this.Button2.Text = "Stop pressing";
+            int MyClicks = 0;
+           
+            if (ViewState["MyClicks"] != null)
+            {
+                MyClicks = (int)ViewState["MyClicks"];
+
+            }
+            this.Label2.Text = (++MyClicks).ToString();
+            ViewState["MyClicks"] = MyClicks;
+        }
+      
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            if (Panel1.Visible == true)
+            {
+                Panel1.Visible = false;
+                Panel2.Visible = true;
+            }
+            else
+            {
+                Panel1.Visible = true;
+                Panel2.Visible = false;
+            }
+            this.ListBox1.Items.Add(new ListItem { Value = 10.ToString(), Text = 10.ToString() });
+        }
+    }
+}
