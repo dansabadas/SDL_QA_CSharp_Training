af1a5eda8e0cca212a930c5d8d3a1529ae2ee0e6
Catalina <ccascaval@sdl.com>
2019-03-06 22:00:57 +0200

    Homework 3 and 4 (Bank and Army)

---
+++ Catalina_Cascaval/Homework3/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            IBank bank = new Bank();
+            ArrayList array = new ArrayList();
+            array = bank.GenerateFinancialTransactions(10);
+            foreach (object obj in array)
+            {
+                var person = obj as FinancialTransaction;
+                Console.WriteLine(person.ToString());
+            }
+
+        }
+    }
+}
