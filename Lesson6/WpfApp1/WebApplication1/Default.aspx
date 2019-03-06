cb2dec9f150d0f94fe992a7b5cbc42d717c9249c
Alin Soritau <asoritau@sdl.com>
2019-03-06 14:33:34 +0200

    done samples with panel and list data

---
+++ Lesson6/WpfApp1/WebApplication1/Default.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication1._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:Panel ID="panel1" runat="server">
+        <asp:Button ID="Button1" runat="server" Text="Press Me!" OnClick="Button1_Click" />
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+    </asp:Panel>
+
+    <asp:ListBox ID="ListBox1" runat="server">
+        <Items>
+            <asp:ListItem runat="server" Text="text 1" Value ="1"></asp:ListItem>
+            <asp:ListItem runat="server" Text="text 1" Value ="1"></asp:ListItem>
+        </Items>
+    </asp:ListBox>
+    <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="Switch Buttons" />
+    <asp:Panel ID="panel2" runat="server">
+        <asp:Button ID="Button2" runat="server" Text="Button 2" OnClick="Button2_Click" />
+        <asp:Label ID="Label2" runat="server" Text="Label"></asp:Label>
+    </asp:Panel>
+    
+</asp:Content>
---
+++ Lesson6/WpfApp1/WebApplication1/Default.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            this.Button1.Text = "I was pressed";
+            int myClicks = 0;
+            if(Session["MyClicks"] != null)
+            {
+                myClicks = (int)Session["MyClicks"];
+            }
+
+            this.Label1.Text = (++myClicks).ToString();
+            Session["MyClicks"] = myClicks;
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            this.Button2.Text = "I was pressed";
+            int myClicks = 0;
+            if (ViewState["MyClicks"] != null)
+            {
+                myClicks = (int)ViewState["MyClicks"];
+            }
+
+            this.Label2.Text = (++myClicks).ToString();
+            ViewState["MyClicks"] = myClicks;
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            if(panel1.Visible == true)
+            {
+                panel1.Visible = false;
+                panel2.Visible = true;
+            }
+            else
+            {
+                panel2.Visible = false;
+                panel1.Visible = true;
+            }
+
+            this.ListBox1.Items.Add(new ListItem { Value = 10.ToString(), Text = "10" });
+        }
+    }
+}
